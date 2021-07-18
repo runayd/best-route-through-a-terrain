0c0d766baf562595831a394c10c8f607b43552be
runayd <runaydhaygude97@gmail.com>
2021-07-18 16:19:03 +0530

    fixes #11 : properly center align the menu-slate

--- src/app/map/directive/free-dragging.directive.ts
+++ src/app/map/directive/free-dragging.directive.ts
@@ -60,9 +60,9 @@ export class FreeDraggingDirective implements AfterViewInit, OnDestroy {
 
     let initialX: number,
       initialY: number,
-      currentX = window.innerWidth/2,
+      currentX = window.innerWidth/2 - (this.element.clientWidth/2),
       currentY = 2*(window.innerHeight/3);
-    
+
     this.element.style.transform =
         'translate3d(' + currentX + 'px, ' + currentY + 'px, 0)';
 
